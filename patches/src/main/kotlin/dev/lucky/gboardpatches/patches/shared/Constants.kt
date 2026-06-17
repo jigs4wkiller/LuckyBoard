3934123d09cc4b4cd21c75fe994e963b31471d21
root <sakulnrob@gmail.com>
2026-06-17 21:20:44 +0000

    Update target Gboard version to 17.5.7.917159154-lite_release-arm64-v8a

--- patches/src/main/kotlin/dev/lucky/gboardpatches/patches/shared/Constants.kt
+++ patches/src/main/kotlin/dev/lucky/gboardpatches/patches/shared/Constants.kt
@@ -23,7 +23,7 @@ internal object Constants {
         ),
         targets = listOf(
             AppTarget(
-                version = "17.0.10.880768217-release-arm64-v8a",
+                version = "17.5.7.917159154-lite_release-arm64-v8a",
                 isExperimental = true
             )
         )
