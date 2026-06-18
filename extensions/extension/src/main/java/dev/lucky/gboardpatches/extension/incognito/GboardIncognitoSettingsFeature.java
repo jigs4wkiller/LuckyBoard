7e469cf748211f9a746803eb77c5e411e0f4a7d5
root <sakulnrob@gmail.com>
2026-06-18 19:35:13 +0000

    Revert "Add Incognito extension classes for runtime settings (GboardIncognitoRuntime, GboardIncognitoSettings, GboardIncognitoSettingsFeature)"
    This reverts commit f7412907566a81fbf833126efedcfb162c7dbac5.

--- extensions/extension/src/main/java/dev/lucky/gboardpatches/extension/incognito/GboardIncognitoSettingsFeature.java
+++
@@ -1,7 +0,0 @@
-package dev.lucky.gboardpatches.extension.incognito;
-
-public final class GboardIncognitoSettingsFeature {
-    public static final String FEATURE_INCognito = "dev.lucky.gboardpatches.feature.incognito_enhancements";
-
-    private GboardIncognitoSettingsFeature() {}
-}
