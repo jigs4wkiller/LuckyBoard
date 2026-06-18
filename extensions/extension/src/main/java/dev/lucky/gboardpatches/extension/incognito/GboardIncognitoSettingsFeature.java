73e8ff4e3edb422cf0428341b3abd1bcfdb94a0c
root <sakulnrob@gmail.com>
2026-06-18 19:17:47 +0000

    Add Incognito extension classes for runtime settings

---
+++ extensions/extension/src/main/java/dev/lucky/gboardpatches/extension/incognito/GboardIncognitoSettingsFeature.java
@@ -0,0 +1,7 @@
+package dev.lucky.gboardpatches.extension.incognito;
+
+public final class GboardIncognitoSettingsFeature {
+    public static final String FEATURE_INCognito = "dev.lucky.gboardpatches.feature.incognito_enhancements";
+
+    private GboardIncognitoSettingsFeature() {}
+}
